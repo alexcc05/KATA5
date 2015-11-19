325e7b8f8f06880f54e5c1a413afa177e98980b1
usuario <usuario@10.22.143.10>
2015-11-19 10:21:19 +0000

    KATA5 (con SQLLITE)

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -5,6 +5,11 @@
  */
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -20,20 +25,32 @@ public class Kata5 {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("oracle.jdbc.driver.OracleDriver"); //DRIVER DE ORACLE
-        Connection connect= DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
+        Class.forName("org.sqlite.JDBC"); //DRIVER DE SQLITE
+        Connection connect= DriverManager.getConnection("jdbc:sqlite:Kata5.db");//Nombre de la db
         //ACCEDEMOS A LA BASE DE DATOS
         Statement state = connect.createStatement();
         String query="SELECT * FROM PEOPLE";
-        
         ResultSet rs= state.executeQuery(query);
         
         while(rs.next()){
-            System.out.println(rs.getInt("ID"));
+            System.out.print(rs.getInt("ID")+" ");
             System.out.println(rs.getString("NAME"));
         }
+        
+        
+        
+        String nameFile="emailsfilev1.txt";
+        BufferedReader reader=new BufferedReader(new FileReader(new File(nameFile)));
+        
+        String mail;
+        while((mail=reader.readLine())!=null){
+            query= "INSERT INTO MAILS(MAIL) VALUES ('"+mail+"')";
+            state.executeUpdate(query);//Para actualizar la tabla
+        }
+        
         rs.close();
         state.close();
         connect.close();
